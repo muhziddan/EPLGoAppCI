5a4e1bbd1102bcf47b6a800e3c694e3eb27d949a
Muhammad Ziddan Hidayatullah <ziddan.mz@gmail.com>
2021-06-13 10:04:07 +0700

    add security and performance

--- core/src/main/java/com/ziddan/eplgo/core/di/CoreModule.kt
+++ core/src/main/java/com/ziddan/eplgo/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.ziddan.eplgo.core.data.source.remote.RemoteDataSource
 import com.ziddan.eplgo.core.data.source.remote.network.ApiService
 import com.ziddan.eplgo.core.domain.repository.IClubRepository
 import com.ziddan.eplgo.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,29 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<ClubDatabase>().clubDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             ClubDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "tourism-api.dicoding.dev"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/ctt1haazs8U6LJbBhG1dMDCxflw6Q5LRFqlJP+iCf3E=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
